8ac923870bcecb254b5bd348b19af374cf83ade6
arctic-kraken <kiepasjakub20@gmail.com>
2026-08-25 15:35:45 +0100

    Removed AppJWT as it was unused

--- Models/Authentication/AppJWT.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace nbs_smart_wallet.Models.Authentication
-{
-	public class AppJWT
-	{
-		public string iss { get; set; } = string.Empty;
-		public string aud { get; set; } = string.Empty;
-		public string signing_key { get; set; } = string.Empty;
-	}
-}
